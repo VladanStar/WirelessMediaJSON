268e973cc48d1e3c2fc2a5d5aa8173277529c84c
unknown <vladancupric@gmail.com>
2022-08-31 14:53:11 +0200

    Add xml http request

--- 2/main.js
+++ 2/main.js
@@ -2,22 +2,22 @@ jQuery(document).ready(function($){
 
     $.ajax({
         url:"https://jsonkeeper.com/b/LUH3",
-        type:"GET",
+        type:"get",
         dataType:"script",
         // data:{param1:"value1"},
     })
 
     .done(function(result){
-        var allData = $(result).find("d");
-        allData.each(function(index,el){
-            $("#wrapp").append('<p><a href="'+$(this).attr('email')+'">'+$(this).attr('name')+'</a></p>')
+    //     var allData = $(result).find("d");
 
-        })
+      /// console.log(allData);
+      result.forEach(function(e,i){
+        console.log(e.name);
 
-       
+      })
         console.log("success");
 
-        console.log(allData);
+
     })
     .fail(function(){
         console.log("error")
---
+++ 3/main.js
@@ -0,0 +1,3 @@
+function run(){
+    var xmlXttp = XMLHttpRequest()
+}
