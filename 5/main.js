6e7aaafc9023163e8b7dbb402ad485d6132436b6
unknown <vladancupric@gmail.com>
2022-08-31 16:56:53 +0200

    Add commits

--- 5/main.js
+++ 5/main.js
@@ -1,4 +1,20 @@
-console.log(fetch('https://jsonkeeper.com/b/LUH3')
-.then((response) => response.json())
-.then((data) => console.log(data))
+fetch('https://jsonkeeper.com/b/LUH3', {
+    method:"POST",
+    headers:{
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+name:"User 1"
+    })
+})
+.then((res) => {
+    if(res.ok){
+        console.log("SUCCES")
+    }
+    else {
+        console.log("NOT Succesful")
+    }
+    return res.json()}
 )
+.then(data => console.log(data))
+.catch(error => console.log("ERROR"))
