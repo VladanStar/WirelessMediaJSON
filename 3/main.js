440a19c2b75e523f3d8719b17e0c17ab0015bc70
unknown <vladancupric@gmail.com>
2022-08-31 15:04:29 +0200

    Add

--- 3/main.js
+++ 3/main.js
@@ -1,3 +1,15 @@
 function run(){
-    var xmlXttp = XMLHttpRequest()
+    var xmlXttp = new XMLHttpRequest();
+    xmlXttp.onreadystatechange = function(){
+        if(xmlXttp.readyState == 4 && xmlXttp.status == 200){
+            data(xmlXttp);
+        }
+    }
+    xmlXttp.open('GET', 'https://jsonkeeper.com/b/LUH3',true);
+    xmlXttp.send();
+}
+function data(dataJSON){
+    var allData = JSON.parse(dataJSON.responseText);
+    console.log(allData);
+
 }
